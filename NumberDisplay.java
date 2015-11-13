ded2f4463280a477f56d562ae74ec2020e2a61c5
David <you@example.com>
2015-11-13 12:13:19 +0100

    Añadido metodo setter

--- NumberDisplay.java
+++ NumberDisplay.java
@@ -1,5 +1,15 @@
+/**Crea un nuevo repositorio en GitHub llamado mi-clock-display.
 
-/**
+ *Luego codifica en BlueJ la clase NumberDisplay realizando los commits que creas oportunos. A continuación se indica qué es necesario que, como mínimo, implemente dicha clase:
+
+ *Un constructor que recibe por parámetro el límite del display y que fija el valor actual del display a 0.
+ *Un método setter que fija el valor actual del display al valor pasado como parámetro (nombre del método: setValue).
+ *Un método que devuelve el valor actual del display en forma de cadena de 2 caracteres (nombre del método: getDisplayValue).
+ *Un método que devuelve el valor actual del display como entero (nombre del método: getValue),
+ *Un método que incrementa en 1 al display y lo hace volver al principio si alcanza el límite (nombre del método: increment)
+ *Una vez que hayas comprobado que funciona, sube el repositorio a GitHub e indica la URL del último commit como respuesta de esta actividad.
+
+ /**
  * Write a description of class NumberDisplay here.
  * 
  * @author (your name) 
@@ -8,26 +18,26 @@
 public class NumberDisplay
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private int limit;
+    private int display;
 
     /**
      * Constructor for objects of class NumberDisplay
      */
-    public NumberDisplay()
+    public NumberDisplay(int maximo)
     {
         // initialise instance variables
-        x = 0;
+        limit = maximo;
+        display = 0;
     }
-
+    
     /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * Un método setter que fija el valor actual del display al valor pasado como parámetro 
      */
-    public int sampleMethod(int y)
+    public void setValue(int value)
     {
-        // put your code here
-        return x + y;
+        if(value >= 0 && value<= limit){
+            display = value;
+        }
     }
 }
